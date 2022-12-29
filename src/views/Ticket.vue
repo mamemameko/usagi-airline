ca23cf8016ecda400f0bac17f9c105445e8a8f0c
akihiroobara <akihiroobara167@gmail.com>
2022-12-30 02:27:31 +0900

    日本語フォントが適用されず文字化けする問題を解消

--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { useTicketDataStore } from '@/stores/ticketData'
 import { jsPDF } from "jspdf";
+import "@/assets/font/Mplus1-Medium-normal-ESModule";
 import PDFObject from "pdfobject";
 
 const store = useTicketDataStore();
 
 const doc = new jsPDF();
-doc.addFont("/Mplus1-Medium.ttf", "Mplus1", "Medium");
-doc.setFont("Mplus1", "Medium");
-doc.text('名前', 10, 10);
-doc.text('住所', 10, 20);
-doc.text('目的地', 10, 30);
+doc.setFont("Mplus1-Medium", "normal");
+doc.text('名前: ' + store.name, 10, 10);
+doc.text('住所: ' + store.address, 10, 20);
+doc.text('目的地: ' + store.destination, 10, 30);
 const pdf = doc.output('datauristring');
 
 PDFObject.embed(pdf);
